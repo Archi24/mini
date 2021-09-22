bf8769111c13d8ac2535ada91333df00d3c37c47
Archi24 <78952113+Archi24@users.noreply.github.com>
2021-09-22 23:45:49 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+var Speechrecognition = window.webkitSpeechRecognition;
+//var GrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+var recognition = new Speechrecognition();
+
+var textbox = $("#rest");
+var instructions = $("#instructions");
+var content = '';
+recognition.continuous=true;
+recognition.onstart = function(){
+    instructions.text("Mic on");
+}
+recognition.onspeechend=function(){
+    instructions.text("No Activity");
+}
+recognition.onerror=function(){
+    instructions.text("Try Again");
+}
+recognition.onresult=function(event)
+{
+    var current=event.resultIndex;
+    var transcript = event.results[current][0].transcript;
+    content+=transcript;
+    textbox.val(content);
+}
+$("#btn").click(function(event){
+    if(content.length)
+    {
+        content+= '';
+    }
+    recognition.start();
+    console.log("hello");
+})
+textbox.on('input',function()
+{
+    content=$(this).val();
+})
